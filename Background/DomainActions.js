dcf8f32b06a4eaaa40578cbdd87de39f64afe584
DeclanEM <54198441+DeclanEM@users.noreply.github.com>
2019-08-17 15:31:00 +1000

    Add files via upload

---
+++ Background/DomainActions.js
@@ -0,0 +1,156 @@
+"use strict";
+
+let extensionName = browser.i18n.getMessage("extensionName");
+let warning_NoTabs = browser.i18n.getMessage("warning_NoTabs");
+
+async function GetSettings() {
+    let value = await browser.storage.local.get(["currentWindow", "bypassCache", "exceptActive"]);
+    return { "currentWindow": value.currentWindow, "bypassCache": value.bypassCache, "exceptActive": value.exceptActive };
+}
+
+function CreateMenuItems() {
+    browser.contextMenus.create({
+        id: "DomainActionsMenuItem",
+        contexts: ["tab"],
+        title: extensionName
+    });
+
+    browser.contextMenus.create({
+        id: "DomainActionsNewWindow",
+        icons: {
+            "16": "Resources/Icons/Icon_NewWindow.svg"
+        },
+        parentId: "DomainActionsMenuItem",
+        contexts: ["tab"],
+        title: browser.i18n.getMessage("menuItem_NewWindow")
+    });
+
+    browser.contextMenus.create({
+        id: "DomainActionsRefreshTabs",
+        icons: {
+            "16": "Resources/Icons/Icon_Refresh.svg"
+        },
+        parentId: "DomainActionsMenuItem",
+        contexts: ["tab"],
+        title: browser.i18n.getMessage("menuItem_Refresh")
+    });
+
+    browser.contextMenus.create({
+        id: "DomainActionsCloseTabs",
+        icons: {
+            "16": "Resources/Icons/Icon_Close.svg"
+        },
+        parentId: "DomainActionsMenuItem",
+        contexts: ["tab"],
+        title: browser.i18n.getMessage("menuItem_Close")
+    });
+}
+
+async function OnClickedListener(info, tab) {
+    let matches = tab.url.match(/^https?\:\/\/(?:www\.)?([^\/:?#]+)(?:[\/:?#]|$)/i);
+    let domain = matches && matches[1];
+
+    if (domain === null) {
+        console.warn(browser.i18n.getMessage("warning_NoDomain"));
+        return;
+    } else {
+        settings = await GetSettings();
+        let querying;
+        if (settings.exceptActive) {
+            querying = browser.tabs.query({ url: ["*://*." + domain + "/*"], active: false, currentWindow: settings.currentWindow });
+        } else {
+            querying = browser.tabs.query({ url: ["*://*." + domain + "/*"], currentWindow: settings.currentWindow });
+        }
+
+        switch (info.menuItemId) {
+            case "DomainActionsNewWindow":
+                querying.then(MoveToNewWindow);
+                break;
+            case "DomainActionsRefreshTabs":
+                querying.then(RefreshTabs);
+                break;
+            case "DomainActionsCloseTabs":
+                querying.then(CloseTabs);
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+function MoveToNewWindow(tabs) {
+    if (tabs.length < 1) {
+        console.warn(warning_NoTabs);
+        return;
+    }
+    let tabIds = tabs.map(tabInfo => tabInfo.id);
+
+    let newWindow = browser.windows.create({
+        state: "maximized",
+        tabId: tabIds.shift()
+    });
+
+    if (tabIds.length < 1) { return; }
+
+    newWindow.then((windowInfo) => {
+        browser.tabs.move(tabIds, { windowId: windowInfo.id, index: -1 });
+    });
+}
+
+function RefreshTabs(tabs) {
+    if (tabs.length < 1) {
+        console.warn(warning_NoTabs);
+        return;
+    }
+    let tabIds = tabs.map(tabInfo => tabInfo.id);
+
+    for (let tabId of tabIds) {
+        browser.tabs.reload(tabId, { "bypassCache": settings.bypassCache });
+    }
+}
+
+function CloseTabs(tabs) {
+    if (tabs.length < 1) {
+        console.warn(warning_NoTabs);
+        return;
+    }
+    let tabIds = tabs.map(tabInfo => tabInfo.id);
+    browser.tabs.remove(tabIds);
+}
+
+function InstalledHandler(details) {
+    if (details.reason === "update") {
+        browser.notifications.create(
+            "UpdateNotification", {
+                "iconUrl": browser.runtime.getURL("Resources/Icons/Icon_96.png"),
+                "message": browser.i18n.getMessage("notification_UpdateText"),
+                "title": extensionName,
+                "type": "basic"
+            });
+    } else if (details.reason === "install") {
+        browser.notifications.create(
+            "InstallNotification", {
+                "iconUrl": browser.runtime.getURL("Resources/Icons/Icon_96.png"),
+                "message": browser.i18n.getMessage("notification_InstallText"),
+                "title": extensionName,
+                "type": "basic"
+            });
+    }
+};
+
+function NotificationClickHandler(id, index) {
+    browser.notifications.clear(id);
+
+    if (id === "UpdateNotification") {
+        browser.tabs.create({ url: browser.runtime.getURL("Pages/Changelog/Changelog.html") });
+    } else if (id === "InstallNotification") {
+        browser.tabs.create({ url: browser.runtime.getURL("Pages/Onboard/Onboard.html") });
+    }
+}
+
+let settings = { "currentWindow": true, "bypassCache": false, "exceptActive": false };
+CreateMenuItems();
+
+browser.contextMenus.onClicked.addListener(OnClickedListener);
+browser.runtime.onInstalled.addListener(InstalledHandler);
+browser.notifications.onClicked.addListener(NotificationClickHandler);
---
+++ Pages/Changelog/Changelog.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function InjectImages() {
+    document.getElementById("1.0.0 Image").setAttribute("src", browser.runtime.getURL("Resources/Icons/Icon_Released.png"));
+    document.getElementById("1.1.0 Image").setAttribute("src", browser.runtime.getURL("Resources/Icons/Icon_Upcoming.png"));
+}
+
+document.addEventListener("DOMContentLoaded", InjectImages);
